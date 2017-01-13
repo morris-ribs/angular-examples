38d11bd30684b25312f6c12e7934ac1928b0ed9d
morris-ribs <maumribeiro@gmail.com>
2017-01-13 20:49:03 +0100

    commiting everything

---
+++ angular-tesla-range-calculator/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// our feature module
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TeslaBatteryModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
